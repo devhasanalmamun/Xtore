ec79042eab77248d8337afca54e389019855f6df
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-07-30 13:21:57 +0600

    added spatie/laravel-data for DTO's and made a RegisterdUserData DTO

---
+++ app/DataTransferObjects/RegisteredUserData.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataTransferObjects;
+
+use Spatie\LaravelData\Attributes\Validation\Confirmed;
+use Spatie\LaravelData\Attributes\Validation\Password;
+use Spatie\LaravelData\Attributes\Validation\Required;
+use Spatie\LaravelData\Attributes\Validation\Unique;
+use Spatie\LaravelData\Attributes\Validation\Email;
+use Spatie\LaravelData\Attributes\Validation\Max;
+use Spatie\LaravelData\Attributes\Hidden;
+use Spatie\LaravelData\Data;
+use App\Models\User;
+
+final class RegisteredUserData extends Data {
+  public function __construct(
+    #[Required, Max(255)]
+    public readonly string $name,
+
+    #[Required, Max(255), Email(Email::NoRfcWarningsValidation), Unique(User::class, 'email')]
+    public readonly string $email,
+
+    #[Required, Max(255), Confirmed, Password(min:8, numbers:true, letters:true)]
+    public readonly string $password,
+
+    #[Hidden]
+    public readonly string $password_confirmation,
+  ){}
+}
