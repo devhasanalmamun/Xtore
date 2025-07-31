b41083c871c9ddd70c76f80901b90f1af8c7e2e8
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-08-01 02:46:42 +0600

    removed that enum in db level so app doesn't explode in such cases cause what if i update my enum my db schema won't update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->enum('role', UserRoleEnum::values())->default(UserRoleEnum::USER->value);
+            $table->string('role')->default(UserRoleEnum::USER->value);
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
