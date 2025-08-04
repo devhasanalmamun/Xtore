4496221eefc79bac2ed79c8511649be5c8f9f0d3
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-08-04 11:41:59 +0600

    changed homepage routing

--- resources/js/pages/landings/homepage/homepage-index.tsx
+++ resources/js/pages/landings/homepage/homepage-index.tsx
@@ -1,6 +1,6 @@
 import LandingsLayout from '@/layouts/landings/landings-layout'
 
-export default function Welcome() {
+export default function HomepageIndex() {
   return (
     <LandingsLayout title="Welcome to Our Site" description="This is the welcome page.">
       <div className="h-[70vh]">Homepage</div>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
+    return Inertia::render('landings/homepage/homepage-index');
 })->name('home');
 
 Route::get('/contact-us', function () {
