fdb9ccaa7682bb3fc00791fc0a4b4a695fb12275
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-08-08 04:15:22 +0600

    added random window tile shows and viewport when created

--- resources/js/pages/landings/test/partials/window-tiler.tsx
+++ resources/js/pages/landings/test/partials/window-tiler.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 interface IWindowTiler {
   id: number
   window_color: string
   heading_color: string
+  offset_top: number
+  offset_left: number
 }
 
 export default function WindowTiler() {
@@ -11,9 +13,19 @@ export default function WindowTiler() {
   const heading_color = useGenerateRandomRGB()
 
   const [windows, setWindows] = useState<IWindowTiler[]>([])
+  const viewportRef = useRef(null)
+
+  // Global consts
+  const window_tile_height = 240
+  const window_tile_width = 320
 
   function addWindow() {
-    const newWindow: IWindowTiler = { id: Date.now(), window_color, heading_color }
+    if (!viewportRef.current) return
+
+    const offset_left = Math.floor(Math.random() * (viewportRef.current.clientWidth - window_tile_width))
+    const offset_top = Math.floor(Math.random() * (viewportRef.current.clientHeight - window_tile_height))
+
+    const newWindow: IWindowTiler = { id: Date.now(), window_color, heading_color, offset_left, offset_top }
     setWindows((prev) => [...prev, newWindow])
   }
 
@@ -22,23 +34,32 @@ export default function WindowTiler() {
   }
 
   return (
-    <section className="h-[100vh] overflow-hidden p-2 text-white">
-      {windows.length > 0 &&
-        windows.map((window) => {
-          return (
-            <div key={window.id} className="h-60 w-80" style={{ backgroundColor: window.window_color }}>
-              <div
-                className="flex items-center justify-between border-b p-2"
-                style={{ backgroundColor: window.heading_color }}
-              >
-                <p>Window</p>
-                <button className="cursor-pointer rounded text-xl font-bold" onClick={() => removeWindow(window.id)}>
-                  x
-                </button>
-              </div>
+    <section ref={viewportRef} className="relative h-[100vh] overflow-hidden bg-slate-800 p-2 text-white">
+      {windows.map((window) => {
+        return (
+          <div
+            key={window.id}
+            className="absolute"
+            style={{
+              height: window_tile_height,
+              width: window_tile_width,
+              backgroundColor: window.window_color,
+              left: window.offset_left,
+              top: window.offset_top,
+            }}
+          >
+            <div
+              className="flex items-center justify-between border-b p-2"
+              style={{ backgroundColor: window.heading_color }}
+            >
+              <p>Window</p>
+              <button className="cursor-pointer rounded text-xl font-bold" onClick={() => removeWindow(window.id)}>
+                x
+              </button>
             </div>
-          )
-        })}
+          </div>
+        )
+      })}
 
       <button
         className="absolute right-0 bottom-0 m-1 cursor-pointer rounded bg-blue-400 px-2 text-3xl text-gray-50 transition-all hover:bg-blue-500"
