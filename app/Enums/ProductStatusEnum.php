f1b6d4cb43b165e1a7bd62d3d1d183af045e41d7
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-08-27 22:52:05 +0600

    added product status enum

---
+++ app/Enums/ProductStatusEnum.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Enums;
+
+enum ProductStatusEnum : string
+{
+    case DRAFT = 'draft';
+    case PUBLISHED = 'published';
+
+    public function label(): string
+    {
+        return match($this) {
+            self::DRAFT => 'Draft',
+            self::PUBLISHED => 'Published'
+        };
+    }
+
+    public static function labels(): array
+    {
+        return array_map(fn(self $case) => [
+            'value' => $case->value,
+            'label' => $case->label()
+        ], self::cases());
+    }
+}
