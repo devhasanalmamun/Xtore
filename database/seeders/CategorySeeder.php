1063b8de69bce3d89009c4924c03d18e560faa33
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-08-27 18:40:22 +0600

    added category seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -9,7 +9,39 @@ class CategorySeeder extends Seeder
 {
     public function run(): void
     {
-        Category::factory()->count(5)->create(['parent_id' => null]);
-        Category::factory()->count(25)->create();
+        Category::factory()->createMany(
+            [
+                [
+                    'department_id' => 1, // Assuming Electronics
+                    'parent_id' => null, 
+                    'name' => 'PC Components' 
+                ],
+                [
+                    'department_id' => 1, // Assuming Electronics
+                    'parent_id' => 1, // Assuming parent category PC Components
+                    'name' => 'Storage Device' 
+                ],
+                [
+                    'department_id' => 1, // Assuming Electronics
+                    'parent_id' => null,
+                    'name' => 'Devices' 
+                ],
+                [
+                    'department_id' => 1, // Assuming Electronics
+                    'parent_id' => 3, // Assuming parent category Devices
+                    'name' => 'Mobile Phone' 
+                ],
+                [
+                    'department_id' => 1, // Assuming Electronics
+                    'parent_id' => 2, // Assuming parent Storage Devices
+                    'name' => 'SSD' 
+                ],
+                [
+                    'department_id' => 1, // Assuming Electronics
+                    'parent_id' => 1, // Assuming parent category PC Components
+                    'name' => 'RAM' 
+                ],
+            ]
+        );
     }
 }
