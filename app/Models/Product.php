5d4ca6c65c87ca3a4c5e3d4155d00a8780ddcf12
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-08-28 16:55:34 +0600

    added relationship for prodects cause it belongs to a department and a category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function department() : BelongsTo 
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function category() : BelongsTo 
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
