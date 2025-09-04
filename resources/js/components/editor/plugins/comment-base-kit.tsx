bf09e56c17aaca3da90a0a60134ffee9bcd69b37
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-09-04 13:55:48 +0600

    refactor

--- resources/js/components/editor/plugins/comment-base-kit.tsx
+++
@@ -1,5 +0,0 @@
-import { BaseCommentPlugin } from '@platejs/comment'
-
-import { CommentLeafStatic } from '@/components/editor/nodes/comment-node-static'
-
-export const BaseCommentKit = [BaseCommentPlugin.withComponent(CommentLeafStatic)]
