0cd912e08f2b282dfc71c4037da29e4da22da065
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-10-07 15:20:44 +0600

    fixed toolbar overlay issue and added a schedular to delete temp files

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,18 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Inspiring;
+use Cloudinary\Api\Admin\AdminApi;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Artisan::command('cloudinary:cleanup-temp', function() {
+    new AdminApi()->deleteAssetsByPrefix('temp/');
+})->purpose('Delete all files inside temp folder on cloudinary');
+
+Schedule::command('inspire')->everyMinute()->sendOutputTo(storage_path('logs/inspire.log'));
+Schedule::command('cloudinary:cleanup-temp')->everyMinute()->sendOutputTo(storage_path('logs/cloudinary.log'));
