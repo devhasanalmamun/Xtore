bcb3d01afab331be20e71e33217811bfcb5a3fbe
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-10-16 13:50:52 +0600

    added product_variations combination table variation_type_options table and a pivot product_variation_type_table

---
+++ database/migrations/2025_10_16_070725_create_product_variations_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use App\Enums\ProductVariationTypeEnum;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+      Schema::create('product_variation_type', function (Blueprint $table) {
+        $table->id();
+        $table->foreignUlid('product_id')->constrained('products')->cascadeOnDelete();
+        $table->foreignId('variation_type_id')->constrained('variation_types')->cascadeOnDelete();
+        $table->string('display_type')->default(ProductVariationTypeEnum::RADIO->value);
+        $table->timestamps();
+      });
+
+      Schema::create('variation_type_options', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('product_variation_type_id')->constrained('product_variation_type')->cascadeOnDelete();
+        $table->string('name');
+        $table->string('slug')->unique();
+        $table->string('value')->nullable();
+        $table->timestamps();
+      });
+
+      Schema::create('product_variations', function (Blueprint $table) {
+        $table->id();
+        $table->foreignUlid('product_id')->constrained('products')->cascadeOnDelete();
+        $table->json('variation_type_options_ids');
+        $table->integer('stock');
+        $table->decimal('price');
+        $table->decimal('discount_percentage')->nullable();
+        $table->json('images');
+        $table->timestamps();
+      });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variations');
+    }
+};
