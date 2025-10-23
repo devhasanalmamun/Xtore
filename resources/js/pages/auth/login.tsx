8017e2f2739c9aa06cec8bb5f60cff3a03e799db
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-10-23 17:29:03 +0600

    enhanced auth ui

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -1,6 +1,6 @@
 import { Head, useForm } from '@inertiajs/react'
+import React, { FormEventHandler } from 'react'
 import { LoaderCircle } from 'lucide-react'
-import { FormEventHandler } from 'react'
 
 import LandingsLayout from '@/layouts/landings/landings-layout'
 import InputError from '@/components/ui/input-error'
@@ -38,73 +38,82 @@ export default function Login({ status, canResetPassword }: LoginProps) {
   return (
     <>
       <Head title="Log in" />
-
-      <div className="mx-auto my-16 max-w-lg rounded-lg bg-gray-50">
-        <form className="flex flex-col gap-6 p-6" onSubmit={submit}>
-          <div className="grid gap-6">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email address</Label>
-              <Input
-                id="email"
-                type="email"
-                required
-                autoFocus
-                tabIndex={1}
-                autoComplete="email"
-                value={data.email}
-                onChange={(e) => setData('email', e.target.value)}
-                placeholder="email@example.com"
-              />
-              <InputError message={errors.email} />
+      <section className="mx-4 my-16 max-w-7xl xl:mx-auto">
+        <div className="rounded-lg md:grid md:grid-cols-2 md:items-center md:bg-gray-50">
+          <img className="hidden h-180 w-full object-cover md:block" srcSet="/assets/images/auth.png" alt="auth" />
+          <form className="flex flex-col gap-6 bg-gray-50 p-4 md:bg-transparent md:p-6" onSubmit={submit}>
+            <div className="flex items-center gap-2">
+              <h2 className="font-mono text-2xl font-semibold text-primary capitalize sm:text-3xl lg:text-4xl xl:text-5xl">
+                Welcome back!!
+              </h2>
+              <img className="h-12 object-cover md:h-20" srcSet="/assets/gifs/icon-lord-smile.gif" alt="smile" />
             </div>
 
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                {canResetPassword && (
-                  <TextLink href={route('password.request')} className="ml-auto text-sm" tabIndex={5}>
-                    Forgot password?
-                  </TextLink>
-                )}
+            <div className="grid gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email address</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  required
+                  autoFocus
+                  tabIndex={1}
+                  autoComplete="email"
+                  value={data.email}
+                  onChange={(e) => setData('email', e.target.value)}
+                  placeholder="email@example.com"
+                />
+                <InputError message={errors.email} />
               </div>
-              <Input
-                id="password"
-                type="password"
-                required
-                tabIndex={2}
-                autoComplete="current-password"
-                value={data.password}
-                onChange={(e) => setData('password', e.target.value)}
-                placeholder="Password"
-              />
-              <InputError message={errors.password} />
+
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  {canResetPassword && (
+                    <TextLink href={route('password.request')} className="ml-auto text-sm" tabIndex={5}>
+                      Forgot password?
+                    </TextLink>
+                  )}
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  tabIndex={2}
+                  autoComplete="current-password"
+                  value={data.password}
+                  onChange={(e) => setData('password', e.target.value)}
+                  placeholder="Password"
+                />
+                <InputError message={errors.password} />
+              </div>
+
+              <div className="flex items-center space-x-3">
+                <Checkbox
+                  id="remember"
+                  name="remember"
+                  checked={data.remember}
+                  onClick={() => setData('remember', !data.remember)}
+                  tabIndex={3}
+                />
+                <Label htmlFor="remember">Remember me</Label>
+              </div>
+
+              <Button type="submit" className="mt-4 w-full" tabIndex={4} disabled={processing}>
+                {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
+                Log in
+              </Button>
             </div>
 
-            <div className="flex items-center space-x-3">
-              <Checkbox
-                id="remember"
-                name="remember"
-                checked={data.remember}
-                onClick={() => setData('remember', !data.remember)}
-                tabIndex={3}
-              />
-              <Label htmlFor="remember">Remember me</Label>
+            <div className="text-center text-sm text-muted-foreground">
+              Don't have an account?{' '}
+              <TextLink href={route('register')} tabIndex={5}>
+                Sign up
+              </TextLink>
             </div>
-
-            <Button type="submit" className="mt-4 w-full" tabIndex={4} disabled={processing}>
-              {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
-              Log in
-            </Button>
-          </div>
-
-          <div className="text-center text-sm text-muted-foreground">
-            Don't have an account?{' '}
-            <TextLink href={route('register')} tabIndex={5}>
-              Sign up
-            </TextLink>
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
+      </section>
 
       {status && <div className="mb-4 text-center text-sm font-medium text-green-600">{status}</div>}
     </>
--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -1,6 +1,6 @@
 import { Head, useForm } from '@inertiajs/react'
 import { LoaderCircle } from 'lucide-react'
-import { FormEventHandler } from 'react'
+import React, { FormEventHandler } from 'react'
 
 import LandingsLayout from '@/layouts/landings/landings-layout'
 import InputError from '@/components/ui/input-error'
@@ -36,105 +36,110 @@ export default function Register() {
   return (
     <>
       <Head title="Register" />
-      <div className="mx-auto my-16 max-w-lg bg-gray-50">
-        <form className="flex flex-col gap-6 p-6" onSubmit={submit}>
-          <div className="grid gap-6">
-            <div className="grid gap-2">
-              <Label htmlFor="first_name">First Name</Label>
-              <Input
-                id="first_name"
-                type="text"
-                required
-                autoFocus
-                tabIndex={1}
-                autoComplete="first_name"
-                value={data.first_name}
-                onChange={(e) => setData('first_name', e.target.value)}
-                disabled={processing}
-                placeholder="First name"
-              />
-              <InputError message={errors.first_name} className="mt-2" />
+      <section className="mx-4 my-16 max-w-7xl xl:mx-auto">
+        <div className="rounded-lg md:grid md:grid-cols-2 md:items-center md:bg-gray-50">
+          <img className="hidden h-180 w-full object-cover md:block" srcSet="/assets/images/auth.png" alt="auth" />
+          <form className="flex flex-col gap-6 bg-gray-50 p-6 md:bg-transparent" onSubmit={submit}>
+            <p></p>
+
+            <div className="grid gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="first_name">First Name</Label>
+                <Input
+                  id="first_name"
+                  type="text"
+                  required
+                  autoFocus
+                  tabIndex={1}
+                  autoComplete="first_name"
+                  value={data.first_name}
+                  onChange={(e) => setData('first_name', e.target.value)}
+                  disabled={processing}
+                  placeholder="First name"
+                />
+                <InputError message={errors.first_name} className="mt-2" />
+              </div>
+
+              <div className="grid gap-2">
+                <Label htmlFor="last_name">Last Name</Label>
+                <Input
+                  id="last_name"
+                  type="text"
+                  required
+                  autoFocus
+                  tabIndex={1}
+                  autoComplete="last_name"
+                  value={data.last_name}
+                  onChange={(e) => setData('last_name', e.target.value)}
+                  disabled={processing}
+                  placeholder="Last name"
+                />
+                <InputError message={errors.last_name} className="mt-2" />
+              </div>
+
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email address</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  required
+                  tabIndex={2}
+                  autoComplete="email"
+                  value={data.email}
+                  onChange={(e) => setData('email', e.target.value)}
+                  disabled={processing}
+                  placeholder="email@example.com"
+                />
+                <InputError message={errors.email} />
+              </div>
+
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  tabIndex={3}
+                  autoComplete="new-password"
+                  value={data.password}
+                  onChange={(e) => setData('password', e.target.value)}
+                  disabled={processing}
+                  placeholder="Password"
+                />
+                <InputError message={errors.password} />
+              </div>
+
+              <div className="grid gap-2">
+                <Label htmlFor="password_confirmation">Confirm password</Label>
+                <Input
+                  id="password_confirmation"
+                  type="password"
+                  required
+                  tabIndex={4}
+                  autoComplete="new-password"
+                  value={data.password_confirmation}
+                  onChange={(e) => setData('password_confirmation', e.target.value)}
+                  disabled={processing}
+                  placeholder="Confirm password"
+                />
+                <InputError message={errors.password_confirmation} />
+              </div>
+
+              <Button type="submit" className="mt-2 w-full" tabIndex={5} disabled={processing}>
+                {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
+                Create account
+              </Button>
             </div>
 
-            <div className="grid gap-2">
-              <Label htmlFor="last_name">Last Name</Label>
-              <Input
-                id="last_name"
-                type="text"
-                required
-                autoFocus
-                tabIndex={1}
-                autoComplete="last_name"
-                value={data.last_name}
-                onChange={(e) => setData('last_name', e.target.value)}
-                disabled={processing}
-                placeholder="Last name"
-              />
-              <InputError message={errors.last_name} className="mt-2" />
+            <div className="text-center text-sm text-muted-foreground">
+              Already have an account?{' '}
+              <TextLink href={route('login')} tabIndex={6}>
+                Log in
+              </TextLink>
             </div>
-
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email address</Label>
-              <Input
-                id="email"
-                type="email"
-                required
-                tabIndex={2}
-                autoComplete="email"
-                value={data.email}
-                onChange={(e) => setData('email', e.target.value)}
-                disabled={processing}
-                placeholder="email@example.com"
-              />
-              <InputError message={errors.email} />
-            </div>
-
-            <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                type="password"
-                required
-                tabIndex={3}
-                autoComplete="new-password"
-                value={data.password}
-                onChange={(e) => setData('password', e.target.value)}
-                disabled={processing}
-                placeholder="Password"
-              />
-              <InputError message={errors.password} />
-            </div>
-
-            <div className="grid gap-2">
-              <Label htmlFor="password_confirmation">Confirm password</Label>
-              <Input
-                id="password_confirmation"
-                type="password"
-                required
-                tabIndex={4}
-                autoComplete="new-password"
-                value={data.password_confirmation}
-                onChange={(e) => setData('password_confirmation', e.target.value)}
-                disabled={processing}
-                placeholder="Confirm password"
-              />
-              <InputError message={errors.password_confirmation} />
-            </div>
-
-            <Button type="submit" className="mt-2 w-full" tabIndex={5} disabled={processing}>
-              {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
-              Create account
-            </Button>
-          </div>
-
-          <div className="text-center text-sm text-muted-foreground">
-            Already have an account?{' '}
-            <TextLink href={route('login')} tabIndex={6}>
-              Log in
-            </TextLink>
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
+      </section>
     </>
   )
 }
