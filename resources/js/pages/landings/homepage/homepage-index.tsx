10214aa02a03e9b2c0ccb8306b0566dd7092c543
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-10-25 15:02:22 +0600

    added carousel mockup

--- resources/js/pages/landings/homepage/homepage-index.tsx
+++ resources/js/pages/landings/homepage/homepage-index.tsx
@@ -1,9 +1,22 @@
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
 import LandingsLayout from '@/layouts/landings/landings-layout'
 
 export default function HomepageIndex() {
   return (
     <LandingsLayout title="Welcome to Our Site" description="This is the welcome page.">
-      <div className="h-[70vh]">Homepage</div>
+      {/* Banner */}
+      <section className="relative px-0 pb-8 sm:px-6 lg:px-8 lg:pb-16">
+        <Carousel className="mx-auto max-w-7xl">
+          <CarouselContent className="h-[40dvh] sm:h-[50dvh] md:h-[70dvh]">
+            <CarouselItem className="bg-pink-500">yo</CarouselItem>
+            <CarouselItem className="bg-pink-500">yo</CarouselItem>
+            <CarouselItem className="bg-pink-500">yo</CarouselItem>
+          </CarouselContent>
+
+          <CarouselNext className="right-2 sm:right-4" />
+          <CarouselPrevious className="left-2 sm:left-4" />
+        </Carousel>
+      </section>
     </LandingsLayout>
   )
 }
