0a6f64e4669886e21f253c8ecddef46e53549812
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-10-27 14:54:55 +0600

    refactored homepage and added todos

--- resources/js/pages/landings/homepage/homepage-index.tsx
+++ resources/js/pages/landings/homepage/homepage-index.tsx
@@ -1,18 +1,7 @@
-import { useState, useEffect, useRef } from 'react'
-import Autoplay from 'embla-carousel-autoplay'
-
-import {
-  Carousel,
-  CarouselApi,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from '@/components/ui/carousel'
 import LandingsLayout from '@/layouts/landings/landings-layout'
-import { cn } from '@/lib/utils'
+import Hero from '@/pages/landings/homepage/sections/hero'
 
-interface IBannerImage {
+interface IHeroImage {
   image: string
   title: string
 }
@@ -23,57 +12,22 @@ interface ICategory {
 }
 
 interface IProps {
-  banner_hero_images: IBannerImage[]
+  banner_hero_images: IHeroImage[]
   categories: ICategory[]
 }
 
 export default function HomepageIndex(props: IProps) {
-  const [api, setApi] = useState<CarouselApi>()
-  const [, setCurrent] = useState(0)
-  const plugin = useRef(Autoplay({ delay: 6000, stopOnInteraction: true }))
-
-  useEffect(() => {
-    if (!api) return
-
-    api.on('select', () => setCurrent(api?.selectedScrollSnap()))
-  }, [api])
-
   return (
     <LandingsLayout title="Welcome to Our Site" description="This is the welcome page.">
-      {/* Banner */}
       <section className="relative px-0 pb-8 sm:px-6 lg:px-8 lg:pb-16">
-        <Carousel
-          className="mx-auto max-w-7xl"
-          opts={{ loop: true }}
-          setApi={setApi}
-          plugins={[plugin.current]}
-          onMouseEnter={() => plugin.current.stop()}
-          onMouseLeave={() => plugin.current.play()}
-        >
-          <CarouselContent className="h-[40dvh] sm:h-[50dvh] md:h-[70dvh]">
-            {props.banner_hero_images.map((item, i) => (
-              <CarouselItem key={i}>
-                <img className="h-full w-full object-cover" src={item.image} alt={item.title} />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselNext className="right-2 sm:right-4" />
-          <CarouselPrevious className="left-2 sm:left-4" />
-
-          <div className="absolute bottom-4 left-1/2 flex -translate-x-1/2 gap-2">
-            {Array.from({ length: api?.scrollSnapList().length || 0 }).map((_, i) => (
-              <div
-                key={i}
-                onClick={() => api?.scrollTo(i)}
-                className={cn(
-                  'h-2.5 w-2.5 cursor-pointer rounded-full bg-primary/20',
-                  api?.selectedScrollSnap() === i && 'bg-primary',
-                )}
-              />
-            ))}
-          </div>
-        </Carousel>
+        <Hero hero_images={props.banner_hero_images} />
       </section>
+
+      {/* TODO: Flash Sale Ref: Daraz*/}
+      {/* TODO: Categories: Daraz*/}
+      {/* TODO: Top sold products */}
+      {/* TODO: Top Vendors */}
+      {/* TODO: Recommended Product Based on Browsing History: Daraz */}
     </LandingsLayout>
   )
 }
---
+++ resources/js/pages/landings/homepage/sections/hero.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useRef, useState } from 'react'
+import Autoplay from 'embla-carousel-autoplay'
+
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
+import { cn } from '@/lib/utils'
+
+interface IHeroImage {
+  image: string
+  title: string
+}
+
+interface IProps {
+  hero_images: IHeroImage[]
+}
+export default function Hero(props: IProps) {
+  const [api, setApi] = useState<CarouselApi>()
+  const [, setCurrent] = useState(0)
+  const plugin = useRef(Autoplay({ delay: 6000, stopOnInteraction: true }))
+
+  useEffect(() => {
+    if (!api) return
+
+    api.on('select', () => setCurrent(api?.selectedScrollSnap()))
+  }, [api])
+
+  return (
+    <Carousel
+      className="mx-auto max-w-7xl"
+      opts={{ loop: true }}
+      setApi={setApi}
+      plugins={[plugin.current]}
+      onMouseEnter={() => plugin.current.stop()}
+      onMouseLeave={() => plugin.current.play()}
+    >
+      <CarouselContent className="h-[40dvh] sm:h-[50dvh] md:h-[70dvh]">
+        {props.hero_images.map((item, i) => (
+          <CarouselItem key={i}>
+            <img className="h-full w-full object-cover" src={item.image} alt={item.title} />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselNext className="right-2 sm:right-4" />
+      <CarouselPrevious className="left-2 sm:left-4" />
+
+      <div className="absolute bottom-4 left-1/2 flex -translate-x-1/2 gap-2">
+        {Array.from({ length: api?.scrollSnapList().length || 0 }).map((_, i) => (
+          <div
+            key={i}
+            onClick={() => api?.scrollTo(i)}
+            className={cn(
+              'h-2.5 w-2.5 cursor-pointer rounded-full bg-primary/20',
+              api?.selectedScrollSnap() === i && 'bg-primary',
+            )}
+          />
+        ))}
+      </div>
+    </Carousel>
+  )
+}
