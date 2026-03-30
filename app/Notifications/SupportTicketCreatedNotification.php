fc69374f1074e56e470aff98754646f218cc45cf
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2026-03-30 15:44:17 +0600

    Implement toMail content for SupportTicketCreatedNotification and set default MAIL_MAILER

--- app/Notifications/SupportTicketCreatedNotification.php
+++ app/Notifications/SupportTicketCreatedNotification.php
@@ -32,18 +32,22 @@ class SupportTicketCreatedNotification extends Notification implements ShouldQue
 
     public function toMail(object $notifiable): MailMessage
     {
-        // Load Category and Created By
+        $ticket = $this->ticket->loadMissing(['category:id,name', 'createdBy:id,first_name,last_name']);
         
         return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
+            ->subject("New Support Ticket #{$ticket->id}")
+            ->line("Subject: {$ticket->subject}")
+            ->line("New support ticket #{$ticket->id} created by {$ticket->createdBy->first_name} {$ticket->createdBy->last_name}")
+            ->line("Category: {$ticket->category->name}")
+            ->line("Description: {$ticket->description}")
+            ->action('View Ticket', url(route('admin.support-tickets.show', $ticket->id)))
+            ->line('Please respond to the ticket as soon as possible.');
     }
 
     public function toArray(object $notifiable): array
     {
         $ticket = $this->ticket->loadMissing(['category:id,name', 'createdBy:id,first_name,last_name']);
-        
+
         return [
             'ticket_id' => $ticket->id,
             'type' => 'support_ticket_created',
